b2cad553179feec899595cecd8d65a3605a7c4d8
boris <beskrovniibv@gmail.com>
2025-10-30 11:12:10 +0000

    Добавил решение задачи A

---
+++ a.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+# A. Лампочки
+
+LOCAL = True
+
+if LOCAL:
+    class Node:
+        def __init__(self, value, left=None, right=None):
+            self.value = value
+            self.right = right
+            self.left = left
+
+
+def solution(root) -> int:
+    return max(root.value, solution(root.left) if root.left else root.value, solution(root.right) if root.right else root.value)
+
+
+def test():
+    node1 = Node(1)
+    node2 = Node(-5)
+    node3 = Node(3, node1, node2)
+    node4 = Node(2, node3, None)
+    assert solution(node4) == 3
+
+
+if __name__ == '__main__':
+    test()
